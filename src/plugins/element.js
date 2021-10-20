8eaa6d21a7e6e15dddfccddf9580a5e4fa5afa33
zx-itcast <zx15344289465@163.com>
2021-10-20 10:46:05 +0800

    完成用户列表页面

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -6,7 +6,32 @@ import 'element-ui/lib/theme-chalk/index.css'
 // // 导入弹框提示组件
 // import { Message } from 'element-ui'
 
-import { Form, FormItem, Button, Input, Message } from 'element-ui'
+import {
+    Form,
+    FormItem,
+    Button,
+    Input,
+    Message,
+    Container,
+    Header,
+    Aside,
+    Main,
+    Menu,
+    Submenu,
+    MenuItem,
+    Breadcrumb,
+    BreadcrumbItem,
+    Card,
+    Row,
+    Col,
+    Table,
+    TableColumn,
+    Switch,
+    Tooltip,
+    Pagination,
+    Dialog,
+    MessageBox,
+} from 'element-ui'
 
 
 
@@ -14,4 +39,27 @@ Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
 Vue.use(Button)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Switch)
+Vue.use(Tooltip)
+Vue.use(Pagination)
+Vue.use(Dialog)
+
+
+
+
 Vue.prototype.$message = Message
+Vue.prototype.$confirm = MessageBox.confirm
